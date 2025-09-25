3f6968d41d932e639b584e20b2dd03ae6270ee94
Oğuzhan AYDIN <ogzhnaydin06@gmail.com>
2025-09-25 20:10:58 +0300

    project begin

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,6 @@
-import { Link } from 'links/entities/link.entity';
 
-import { CreateLinkDto } from 'links/dto/create-link.dto';
-import { UpdateLinkDto } from 'links/dto/update-link.dto';
 
 export const links = {
-  dto: {
-    CreateLinkDto,
-    UpdateLinkDto,
-  },
-  entities: {
-    Link,
-  },
+  dto: {},
+  entities: {},
 };
