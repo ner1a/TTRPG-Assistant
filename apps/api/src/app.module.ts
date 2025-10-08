eeeda7563304a4ded8026dd5ac5b8146b6e64307
Oğuzhan AYDIN <ogzhnaydin06@gmail.com>
2025-10-08 18:22:24 +0300

    mongodb connection added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
+
 @Module({
+  
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+    
     ThrottlerModule.forRoot([{ ttl: 60_000, limit: 120 }]),
+    
     UsersModule
   ],
 })
