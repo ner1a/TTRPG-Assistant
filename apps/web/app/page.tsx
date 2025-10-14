d7ea634a96b3407a7e19ec95646797c55aef37cd
Oğuzhan AYDIN <ogzhnaydin06@gmail.com>
2025-10-15 00:51:09 +0300

    login call and login form added for test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,24 @@
-import Image, { type ImageProps } from 'next/image';
+"use client";
+import LoginForm from '../components/loginForm';
 import styles from './page.module.css';
 
 export default function Home() {
+
+  async function handleLogin(email: string, password: string) {
+    
+    const res = await fetch(`http://localhost:3000/auth/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    console.log('Response data:', data);
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to TTRPG Assistant!</h1>
+        <LoginForm handleLogin={handleLogin} />
       </main>
     </div>
   );
