40439fcba75fb60b13047415bf4d3c520408186b
Oğuzhan AYDIN <ogzhnaydin06@gmail.com>
2025-10-18 03:24:21 +0300

    we add links for our slug to details pages

--- apps/web/app/characters/page.tsx
+++ apps/web/app/characters/page.tsx
@@ -1,10 +1,16 @@
 import { cookies } from 'next/headers';
 import ListItem from '../../components/listItem';
 import { Character } from '@repo/types';
+import { getUser } from '../../lib/auth';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 const API = process.env.NEXT_PUBLIC_API_URL;
 
 export default async function CharactersPage() {
+    const user = await getUser();
+    if (!user) redirect('/');
+
   const acc = (await cookies()).get('acc')?.value;
   if (!acc) return null;
   const res = await fetch(`${API}/characters`, {
@@ -12,12 +18,15 @@ export default async function CharactersPage() {
     cache: 'no-store',
   });
   const data = await res.json();
+  console.log(data)
   return (
       <>
         <section className="m-auto flex justify-center flex-col items-center py-4">
           {res.ok && data.length > 0 ? (
             data.map((character: Character) => (
-              <ListItem key={character._id}>{character.name}</ListItem>
+              <Link href={"/characters/" + (character.name)} key={character._id}>
+                <ListItem>{character.name}</ListItem>
+              </Link>
             ))
           ) : (
             <h1>You have not any characters yet!</h1>
--- apps/web/app/games/page.tsx
+++ apps/web/app/games/page.tsx
@@ -1,10 +1,16 @@
 import { cookies } from 'next/headers';
 import ListItem from '../../components/listItem';
 import { Game } from '@repo/types';
+import { getUser } from '../../lib/auth';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
 
 const API = process.env.NEXT_PUBLIC_API_URL;
 
 export default async function GamesPage() {
+  const user = await getUser();
+  if (!user) redirect('/');
+
   const acc = (await cookies()).get('acc')?.value;
   if (!acc) return null;
   const res = await fetch(`${API}/game`, {
@@ -13,13 +19,14 @@ export default async function GamesPage() {
   });
 
   const data = await res.json();
-  console.log(data);
   return (
     <>
       <section className="m-auto flex justify-center flex-col items-center py-4">
         {res.ok && data.length > 0 ? (
           data.map((game: Game) => (
-            <ListItem key={game._id}>{game.title}</ListItem>
+            <Link href={'/characters/' + game.title} key={game._id}>
+              <ListItem>{game.title}</ListItem>
+            </Link>
           ))
         ) : (
           <h1>You have not any games yet!</h1>
